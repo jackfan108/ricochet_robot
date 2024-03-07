76ce9fb62c4c868861ff5bff2b3e0b7d76f6583d
Jack Fan <jack.fan@airbnb.com>
2024-03-07 03:14:48 -0500

    generate corners

--- main.py
+++ main.py
@@ -3,19 +3,27 @@ import numpy as np
 import random
 
 LINEWIDTH = 3
+COLOR = 'black'
 
-def generate_wall(x1, y1, x2, y2):
+# randomly generates a point and chooses one of 4 orientations to extend 2 walls
+def generate_corner(x1, y1, x2, y2):
     x = random.randint(x1+1, x2-1)
     y = random.randint(y1+1, y2-1)
     n = random.random()
     if n <= 0.25:
-        return ([x, x+1], [y, y])
+        plt.plot([x, x+1], [y, y], linewidth=LINEWIDTH, color=COLOR)
+        plt.plot([x, x], [y, y-1], linewidth=LINEWIDTH, color=COLOR)
     elif n <= 0.5:
-        return ([x, x], [y, y+1])
+        plt.plot([x, x], [y, y+1], linewidth=LINEWIDTH, color=COLOR)
+        plt.plot([x, x+1], [y, y], linewidth=LINEWIDTH, color=COLOR)
     elif n <= 0.75:
-        return ([x, x-1], [y, y])
+        plt.plot([x, x-1], [y, y], linewidth=LINEWIDTH, color=COLOR)
+        plt.plot([x, x], [y, y+1], linewidth=LINEWIDTH, color=COLOR)
     else:
-        return ([x, x], [y, y-1])
+        plt.plot([x, x], [y, y-1], linewidth=LINEWIDTH, color=COLOR)
+        plt.plot([x, x-1], [y, y], linewidth=LINEWIDTH, color=COLOR)
+
+
 
 def generate_target():
     x = random.randint(0, 15)
@@ -30,25 +38,24 @@ if __name__ == "__main__":
     ax = fig.add_subplot(1, 1, 1)
 
     # draw the boarder
-    plt.plot([0, 0], [16, 0], linewidth=LINEWIDTH, color='black')
-    plt.plot([16, 0], [16, 16], linewidth=LINEWIDTH, color='black')
-    plt.plot([16, 16], [0, 16], linewidth=LINEWIDTH, color='black')
-    plt.plot([0, 16], [0, 0], linewidth=LINEWIDTH, color='black')
+    plt.plot([0, 0], [16, 0], linewidth=LINEWIDTH, color=COLOR)
+    plt.plot([16, 0], [16, 16], linewidth=LINEWIDTH, color=COLOR)
+    plt.plot([16, 16], [0, 16], linewidth=LINEWIDTH, color=COLOR)
+    plt.plot([0, 16], [0, 0], linewidth=LINEWIDTH, color=COLOR)
 
     # draw the central 2x2 square
-    plt.plot([7, 7], [7, 9], linewidth=LINEWIDTH, color='black')
-    plt.plot([7, 9], [9, 9], linewidth=LINEWIDTH, color='black')
-    plt.plot([9, 9], [9, 7], linewidth=LINEWIDTH, color='black')
-    plt.plot([9, 7], [7, 7], linewidth=LINEWIDTH, color='black')
+    plt.plot([7, 7], [7, 9], linewidth=LINEWIDTH, color=COLOR)
+    plt.plot([7, 9], [9, 9], linewidth=LINEWIDTH, color=COLOR)
+    plt.plot([9, 9], [9, 7], linewidth=LINEWIDTH, color=COLOR)
+    plt.plot([9, 7], [7, 7], linewidth=LINEWIDTH, color=COLOR)
 
-    # generate wall_num wall segements in each quadrant
-    wall_num = 12
-    while wall_num > 0:
-        plt.plot(*generate_wall(0, 0, 8, 8), linewidth=LINEWIDTH, color='black')
-        plt.plot(*generate_wall(0, 8, 8, 16), linewidth=LINEWIDTH, color='black')
-        plt.plot(*generate_wall(8, 8, 16, 16), linewidth=LINEWIDTH, color='black')
-        plt.plot(*generate_wall(8, 0, 16, 8), linewidth=LINEWIDTH, color='black')
-        wall_num -= 1
+    # generate corners in each quadrant
+    corner_count = 7
+    for _ in range(corner_count):
+        generate_corner(0, 0, 8, 8)
+        generate_corner(0, 8, 8, 16)
+        generate_corner(8, 8, 16, 16)
+        generate_corner(8, 0, 16, 8)
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('Customizable 2D Grid')
